ed805f222f5c56736eb2469338ae93f381a6c192
Snizze <lypank99@gmail.com>
2019-09-12 12:09:00 -0400

    altered code to work as a cmd app

--- calc.py
+++ calc.py
@@ -1,25 +1,30 @@
 from math import floor
 
 # Stat Equivalents
-one_as = 1.08              # 1 All Stat = x STR
-one_percent_as = 8.31      # 1% All Stat = x STR
-dex_to_str = 12.04         # x DEX = 1 STR
-dex_to_str_percent = 8.06  # x DEX% = 1% STR
-one_atk = 4.06             # 1 ATK = x STR
+# one_as = 1.08              # 1 All Stat = x STR
+# one_percent_as = 8.31      # 1% All Stat = x STR
+# dex_to_str = 12.04         # x DEX = 1 STR
+# one_atk = 4.06             # 1 ATK = x STR
 
-# Flame Stats
-str_amt = 60  # Amount of STR
-dex_amt = 20  # Amount of DEX
-atk_amt = 3   # Amount of ATK
-as_amt = 5    # Amount of All Stat %
+# Input Stat Equivalents
+input_one_as = float(input("How much is one all stat equivalent to in main stat: "))
+input_one_percent_as = float(input("How much is one all stat% equivalent to in main stat: "))
+input_main_to_secondary = float(input("How much secondary stat is equivalent to one main stat: "))
+input_one_atk = float(input("How much main stat is one attack equivalent to: "))
+
+# Input Flame Stats
+input_main_amt = float(input("How much main stat is the flame worth: "))
+input_secondary_amt = float(input("How much secondary stat is the flame worth: "))
+input_atk_amt = float(input("How much attack is the flame worth: "))
+input_as_amt = float(input("How much all stat% is the flame worth: "))
 
 
-total_strength = str_amt + floor(as_amt * one_percent_as)
-if dex_amt > 0:
-    dex_equivalent = floor(dex_amt / dex_to_str)
-    total_strength += dex_equivalent
-if atk_amt > 0:
-    atk_equivalent = floor(atk_amt * one_atk)
+total_strength = input_main_amt + floor(input_as_amt * input_one_percent_as)
+if input_secondary_amt > 0:
+    secondary_equivalent = floor(input_secondary_amt / input_main_to_secondary)
+    total_strength += secondary_equivalent
+if input_atk_amt > 0:
+    atk_equivalent = floor(input_atk_amt * input_one_atk)
     total_strength += atk_equivalent
 
-print(floor(total_strength))
+print(f"Your flame is equivalent to {floor(total_strength)} main stat")
